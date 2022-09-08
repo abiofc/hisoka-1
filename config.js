72f65ccbd285855f0eec3daa96fdc9fc8f6edc1f
Raihann71 <78335100+Raihann71@users.noreply.github.com>
2022-09-08 21:26:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -24,25 +24,25 @@ global.APIKeys = {
 
 //—————「 Set Nama Bot & Own & Agama 」—————//
 //kok pake agama bang? fitur toleransi bang
-global.namabot = 'Akame'
-global.namaowner = 'GuaAbuzz'
+global.namabot = 'Zoora'
+global.namaowner = 'NjirRaihann'
 global.agama = 'Islam'
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6289636827082']
-global.premium = ['6289636827082']
+global.owner = ['6281213671637']
+global.premium = ['6281213671637']
 
 //—————「 Setting Nomor Donasi 」—————//
 //ubah aja kalau ada yg gapunya kasih tanda -
-global.telkomsel = ['6282171978174']
+global.telkomsel = ['6281213671637']
 global.xl = ['-']
 global.indosat = ['-']
-global.tri = ['6289636827082']
+global.tri = ['-']
 global.smartfren = ['-']
-global.gopay = ['6289636827082']
-global.dana = ['6289636827082']
+global.gopay = ['6281213671637']
+global.dana = ['6281213671637']
 global.ovo = ['-']
-global.saweria = ['https://saweria.co/GuaAbuzz']
+global.saweria = ['-']
 
 //—————「 Set Kebutuhan Button 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :)
@@ -51,7 +51,7 @@ global.scbot = 'https://github.com/Abuzzpoet/Akame'
 global.myweb = 'https://tiktok.com/@guaabuzz'
 
 //—————「 Set Wm 」—————//
-global.packname = 'GuaAbuzz'
+global.packname = 'Zoora'
 global.author = 'WhatsApp Bot'
 
 //—————「 Set Nama Session 」—————//
